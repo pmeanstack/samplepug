747054ffc7d8c26586822f6a1b370c0929d7633b
srimeghana <50864504+srimeghana@users.noreply.github.com>
2019-06-04 11:10:49 +0530

    ok

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -5,4 +5,20 @@ $(document).ready(function(){
    	location.reload('/');
    });
    });
+   $(".edit").click(function(){
+   var id=$(this).val();
+   $.post("/edit",{no:id},function(data){
+   	var a=JSON.stringify(data);
+      var pd=JSON.parse(a);
+      alert(pd[0].name);
+      $("#name").val(pd[0].name);
+      $("#email").val(pd[0].email); 
+      $("#rollno").val(pd[0].rollno);
+      $("#college").val(pd[0].college);
+      $("#branch").val(pd[0].branch);
+      $("#address").val(pd[0].address);
+      $("#phoneno").val(pd[0].mobileno);
+   });
+   $(".dontshow").show();
+   });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,91 @@
 var express = require('express');
 var router = express.Router();
+var moment=require('moment');
+var multer=require('multer');
+var nodemailer=require('nodemailer');
+var randomstring=require('randomstring');                                     
 var monk = require('monk');
 var db = monk('localhost:27017/megha'); 
 var collection = db.get('signup'); 
-var collection1 = db.get('regform');          
+var collection1 = db.get('regform');  
+var upload = multer({ dest: 'uploads/' })        
 console.log("connected");                                                    
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+router.get('/forget', function(req, res, next) {
+  res.render('forget');
+});
+router.post('/forpass',function(req,res){
+    var email=req.body.email;
+    console.log(email);
+    var otp=randomstring.generate(5);
+    var msg="<html><head></head><body><b>"+otp+"</b></body></html>"
+    var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+    port: 587,
+    ssl: true,
+  auth: {
+    user: 'msssm0808@gmail.com',
+    pass: '123meghana'
+  }
+});
+
+var mailOptions = {
+  from: 'msssm0808@gmail.com',
+  to: req.body.email,
+  subject: 'Successfully signed in',
+  html:msg
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+collection.update({"email":email},{$set:{"pwd":otp}});
+res.redirect("/")
+});
+ // getting data from db to frontpage
 router.get('/homef', function(req, res, next) {
 	collection1.find({},function(err,docs){
 		console.log(docs);
 		res.locals.data=docs;        
 		res.render('homef');    
 	});
-  
 });
-router.post('/remove',function(req,res){
-	var id=req.body.no;
-	console.log(id);
-	collection1.remove({"rollno":id},function(err,docs){
-		res.send(docs);
-    });
+//sending data to backend
+router.post('/signup',function(req,res){
+	  var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+    port: 587,
+    ssl: true,
+  auth: {
+    user: 'msssm0808@gmail.com',
+    pass: '123meghana'
+  }
 });
 
-router.post('/signup',function(req,res){
+var mailOptions = {
+  from: 'msssm0808@gmail.com',
+  to: req.body.email,
+  subject: 'Successfully signed in',
+  text: 'Thanks for signing into our app'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
 	var data={
 	name:req.body.name,
     email:req.body.email,
@@ -34,6 +94,7 @@ router.post('/signup',function(req,res){
 	address:req.body.address,
 	pwd:req.body.password
     }
+  
 	collection.insert(data,function(err,data){
 		if(err){
 			console.log("error");
@@ -44,11 +105,58 @@ router.post('/signup',function(req,res){
 	});
 	res.redirect("/");
 });
+// to remove a documment
+router.post('/remove',function(req,res){
+	var id=req.body.no;
+	console.log(id);
+	collection1.remove({"rollno":id},function(err,docs){
+		res.send(docs);
+    });
+});
+// to edit a document
+router.post('/edit',function(req,res){
+	var id=req.body.no;
+	console.log(id);
+	collection1.find({"rollno":id},function(err,docs){
+		res.send(docs);
+    });
+
+});
+//to update a document
+router.post('/update',function(req,res){
+	// console.log(req.body.name);
+ //    console.log(req.body.email);
+ //    console.log(req.body.mobileno);
+ //    console.log(req.body.gender);
+ //    console.log(req.body.addres);
+ //    console.log(req.body.college);
+ //    console.log(req.body.rollno);
+ //    console.log(req.body.branch);
+	var data={
+	 name:req.body.name,
+	 email:req.body.email,
+	 mobileno:req.body.mobileno,
+	 gender:req.body.gender,
+	 address:req.body.address,
+	 college:req.body.college,
+	 rollno:req.body.rollno,
+	 branch:req.body.branch 
+	}
+
+	collection1.update({"rollno":req.body.rollno},{$set:data},function(err,docs){
+     res.redirect("/homef");
+});
+});
+
+//login functionality
 router.post('/login',function(req,res){
 	var name=req.body.name;
 	console.log(name);
 	var password=req.body.password;
 	console.log(password);
+    var logintime=moment().format('hh:mm:ss a');
+    console.log(logintime);
+    collection.update({"name":name},{$set:{"lastlogin":logintime}});
     collection.findOne({"name":name,"pwd":req.body.password },function(err,docs){
     	if(!docs){
     		console.log("mismatch");
@@ -63,28 +171,33 @@ router.post('/login',function(req,res){
     	}
     });
 });
-
-router.post('/regform',function(req,res){
+//registrationform
+router.post('/regform', upload.single('image'),function(req,res){
+	console.log(req.file);
 	var fname=req.body.name;
 	console.log(fname);
-	var femail=req.body.email;
-	console.log(femail);
-	var num=req.body.mobileno;
-	console.log(num);
-	var gen=req.body.gender;
-	console.log(gen);
-	var add=req.body.address;
-	console.log(add);
-	var clz=req.body.college;
-	console.log(clz);
-	var roll=req.body.rollno;
-	console.log(roll);
-	var bran=req.body.branch;
-	console.log(bran);
-	collection1.insert({"branch":bran,"rollno":roll,"college":clz,"name":fname,"email":femail,"mobileno":num,"gender":gen,"address":add});
-	res.redirect("/homef");
+	var email=req.body.email;
+	console.log(email);
+	var mobileno=req.body.mobileno;
+	console.log(mobileno);
+	var gender=req.body.gender;
+	console.log(gender);
+	var address=req.body.address;
+	console.log(address);
+    var college=req.body.college;
+	console.log(college);
+	var rollno=req.body.rollno;
+	console.log(rollno);
+	var branch=req.body.branch;
+	console.log(branch);
+	collection1.insert({"branch":branch,"rollno":rollno,"college":college,"name":fname,"email":email,"mobileno":mobileno,"gender":gender,"address":address
 	
 });
+ res.redirect("/homef");
+});
+
+ module.exports = router;
 
 
-module.exports = router;
+
+
