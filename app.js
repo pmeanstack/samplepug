a9bdd43767fdbbeb7903daa5d58bc31697262507
srimeghana <50864504+srimeghana@users.noreply.github.com>
2019-06-08 12:14:00 +0530

    ok

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session=require('client-sessions');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,7 +19,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({
+  cookieName: 'session', // cookie name dictates the key name added to the request object
+  secret: 'blargadeeblargblarg', // should be a large unguessable string
+  duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
+  // activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+  // cookie: {
+  //   path: '/api', // cookie will only be sent to requests under '/api'
+  //   maxAge: 60000, // duration of the cookie in milliseconds, defaults to duration above
+    ephemeral: true, // when true, cookie expires when the browser closes
+    httpOnly: true, // when true, cookie is not accessible from javascript
+    secure: true // when true, cookie will only be sent over SSL. use key 'secureProxy' instead if you handle SSL not in your node process
+  // }
+}));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,59 @@
 var express = require('express');
 var router = express.Router();
 var moment=require('moment');
-var multer=require('multer');
 var nodemailer=require('nodemailer');
 var randomstring=require('randomstring');                                     
 var monk = require('monk');
 var db = monk('localhost:27017/megha'); 
 var collection = db.get('signup'); 
-var collection1 = db.get('regform');  
-var upload = multer({ dest: 'uploads/' })        
+var collection1 = db.get('regform'); 
+var multer=require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname )
+  }
+});
+var upload = multer({ storage: storage })
+// var upload = multer({ dest: 'uploads/' })        
 console.log("connected");                                                    
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+	if(req.session && req.session.user){
+      res.locals.user=req.session.user
+      res.render('/homef');  		
+	}
+	else{
+		req.session.reset();
+		res.render('index');
+	}
 });
 
+router.get('/changepwd', function(req, res, next) {
+  res.render('changepwd');
+});
+router.post('/cp',function(req,res){
+	console.log(req.body);
+	collection.update({"pwd":req.body.oldPassword},{$set:{"pwd":req.body.newPassword}});
+	res.redirect('/homef')
+});
 router.get('/forget', function(req, res, next) {
   res.render('forget');
+});
+router.get('/logout',function(req,res){
+	req.session.reset();
+	res.redirect('/')
 });
 router.post('/forpass',function(req,res){
     var email=req.body.email;
     console.log(email);
-    var otp=randomstring.generate(5);
+    collection.findOne({"email":req.body.email},function(err,docs){
+    	if(docs){
+    		console.log(docs);
+        var otp=randomstring.generate(5);
+    
     var msg="<html><head></head><body><b>"+otp+"</b></body></html>"
     var transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -49,15 +81,27 @@ transporter.sendMail(mailOptions, function(error, info){
   }
 });
 collection.update({"email":email},{$set:{"pwd":otp}});
-res.redirect("/")
+res.redirect("/");
+}
+ else{
+    	console.log("email not found");
+    	res.redirect("/forget");
+    }
+});
 });
  // getting data from db to frontpage
 router.get('/homef', function(req, res, next) {
+	if(req.session && req.session.user){
+      res.locals.user=req.session.user
 	collection1.find({},function(err,docs){
 		console.log(docs);
 		res.locals.data=docs;        
 		res.render('homef');    
 	});
+    }
+    else{
+    	res.redirect('/')
+    }
 });
 //sending data to backend
 router.post('/signup',function(req,res){
@@ -164,6 +208,8 @@ router.post('/login',function(req,res){
     	}
     	else if(docs){
     		console.log("success");
+    		delete docs.password;
+    		req.session.user=docs
     		res.redirect('/homef');
     	}
     	else{
@@ -190,7 +236,8 @@ router.post('/regform', upload.single('image'),function(req,res){
 	console.log(rollno);
 	var branch=req.body.branch;
 	console.log(branch);
-	collection1.insert({"branch":branch,"rollno":rollno,"college":college,"name":fname,"email":email,"mobileno":mobileno,"gender":gender,"address":address
+	var image=req.file.originalname;
+	collection1.insert({"image":image,"branch":branch,"rollno":rollno,"college":college,"name":fname,"email":email,"mobileno":mobileno,"gender":gender,"address":address
 	
 });
  res.redirect("/homef");
